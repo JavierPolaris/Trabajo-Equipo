8a4f45a5db1c2bfcc2c40e1648d9112dc5b7aa98
JavierPolaris <javierrojocanton@gmail.com>
2022-05-26 15:41:52 +0200

    termino login

--- js/login.js
+++ js/login.js
@@ -1,41 +1,73 @@
 
 
 
-//Creo un JSON con los usuarios predefinidos
 
+localStorage.setItem(`clientes: 0`, JSON.stringify({
+  "nombre": "Javier Garcia-Rojo",
+  "edad": 35,
+  "userName": "Polaris",
+  "password": "1234",
+  "Emails": "javi@gmail.com",
+  "peliculas": [{
+    "titulo": "",
+    "fecha": "",
+    "horario": "",
+    "nº entradas": "",
+    "sala": "",
+
+  }]
+
+}))
+localStorage.setItem(`clientes: 1`, JSON.stringify({
+  "nombre": "Yann Poirot",
+  "edad": 30,
+  "userName": "Yann",
+  "password": "4321",
+  "emails": "yaan@gmail.com",
+  "peliculas": [{
+    "titulo": "",
+    "fecha": "",
+    "horario": "",
+    "nº entradas": "",
+    "sala": "",
+
+  }]
+
+}))
+//Creo un JSON con los usuarios predefinidos
 let userReg = [
-    {
-      "nombre": "Javier Garcia-Rojo",
-      "edad": 35,
-      "userName": "Polaris",
-      "password": "1234",
-      "Emails":  "javi@gmail.com",
-      "peliculas": [{
-        "titulo": "",
-        "techa": "",
-        "horario": "",
-        "nº entradas": "",
-        "sala": "",
+  {
+    "nombre": "Javier Garcia-Rojo",
+    "edad": 35,
+    "userName": "Polaris",
+    "password": "1234",
+    "Emails": "javi@gmail.com",
+    "peliculas": [{
+      "titulo": "",
+      "fecha": "",
+      "horario": "",
+      "nº entradas": "",
+      "sala": "",
     }]
-      
-       
-      
-    },
-    {
-     "nombre": "Yann Poirot",
-      "edad": 30,
-      "userName": "Yann",
-      "password": "4321",
-      "emails":  "yaan@gmail.com",
-      "peliculas": [{
-        "titulo": "",
-        "fecha": "",
-        "horario": "",
-        "nº entradas": "",
-        "sala": "",
+
+
+
+  },
+  {
+    "nombre": "Yann Poirot",
+    "edad": 30,
+    "userName": "Yann",
+    "password": "4321",
+    "emails": "yaan@gmail.com",
+    "peliculas": [{
+      "titulo": "",
+      "fecha": "",
+      "horario": "",
+      "nº entradas": "",
+      "sala": "",
     }]
-      }
-  ]
+  }
+]
 
 
 
@@ -46,51 +78,51 @@ let userReg = [
 
 //   Creo la funcion del boton logIn y si estan vacios los imputs o si estan registrados
 
-  document.getElementById('logIn').addEventListener('click', (e)=> {
-      e.preventDefault()
+document.getElementById('logIn').addEventListener('click', (e) => {
+  e.preventDefault()
 
-      let userLog = document.getElementById('usuario').value;
-      let passLog = document.getElementById('pass').value;
+  let userLog = document.getElementById('usuario').value;
+  let passLog = document.getElementById('pass').value;
 
-      if (userLog == '' || passLog == '') {
-        alert("Los campos usuario y contraseña no pueden estar vacios")
-      }
-      if( (userLog === userReg[0].userName && passLog === userReg[0].password) || (userLog === userReg[1].userName && passLog === userReg[1].password)) {
-         console.log('ok log')
-         location.assign("prueba.html");
-        
-      }else{
-          console.log('no estas registrado')
-         
-      }
-      
-     
-
-    
-    })
-// creo el boton registro para redireccionarnos al registro
-   document.getElementById('registrarse').addEventListener('click', ()=> {
-    mostrar(registro)
-    btnGr(log)
-   })
-
-
-    function mostrar(id) {
-      let test = document.getElementById('registro');
-      if (test.style.display == 'block') {
-          test.style.display = 'none';
-      } else {
-          test.style.display = 'block'
-      }
-    }
-    function btnGr(id) {
-      let test = document.getElementById('log');
-      if (test.style.display == 'inline') {
-          test.style.display = 'block';
-      } else {
-          test.style.display = 'none'
-      }
+  if (userLog == '' || passLog == '') {
+    alert("Los campos usuario y contraseña no pueden estar vacios")
   }
+  if ((userLog === userReg[0].userName && passLog === userReg[0].password) || (userLog === userReg[1].userName && passLog === userReg[1].password)) {
+    console.log('ok log')
+    location.assign("tucine.html");
+
+  } else {
+    console.log('no estas registrado')
+
+  }
+
+
+
+
+})
+// creo el boton registro para redireccionarnos al registro
+document.getElementById('registrarse').addEventListener('click', () => {
+  mostrar(registro)
+  btnGr(log)
+})
+
+
+function mostrar(id) {
+  let test = document.getElementById('registro');
+  if (test.style.display == 'block') {
+    test.style.display = 'none';
+  } else {
+    test.style.display = 'block'
+  }
+}
+function btnGr(id) {
+  let test = document.getElementById('log');
+  if (test.style.display == 'inline') {
+    test.style.display = 'block';
+  } else {
+    test.style.display = 'none'
+  }
+}
 
 // TODO-------------------final LOG
 
@@ -119,25 +151,25 @@ const emailExp = new RegExp(/^([\d\w_\.-]+)@([\d\w\.-]+)\.([\w\.]{3})$/);
  * pero si la contraseña es incorrecta, se devolverá un mensaje.
  */
 const checkPassFormat = (pass) => {
-  if(pass.length < 8)
+  if (pass.length < 8)
     return 'La contraseña debe tener al menos 8 caracteres.';
 
-  if(pass.length > 15)
+  if (pass.length > 15)
     return 'La contraseña no debe exceder los 15 caracteres.';
 
   // comprobar la contraseña tiene una letra mayúscula
-  if(!/[A-Z]/.test(pass))
+  if (!/[A-Z]/.test(pass))
     return 'La contraseña debe contener al menos una letra mayúscula';
 
   // comprobar la contraseña tiene una letra minúscula
-  if(!/[a-z]/.test(pass))
+  if (!/[a-z]/.test(pass))
     return 'La contraseña debe contener al menos una letra mayúscula';
 
   // comprobar la contraseña tiene un dígito
-  if(!/[0-1]/.test(pass))
+  if (!/[0-1]/.test(pass))
     return 'La contraseña debe contener al menos un dígito';
 
-  if(/\ /.test(pass))
+  if (/\ /.test(pass))
     return 'La contraseña no puede tener espacios en blanco';
 
   return '';
@@ -147,65 +179,65 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // verifica el formato de usuario
-  if(!userExp.test(formUser.value)){
+  if (!userExp.test(formUser.value)) {
     alert('Usuario no valido'); return;
   }
 
   // verifica el formato del Email
-  if(!emailExp.test(formEmail.value)){
+  if (!emailExp.test(formEmail.value)) {
     alert('Email no valido'); return;
   }
 
   // el correo electrónico de verificación y el correo electrónico de confirmación son iguales
-  if(formEmail.value.localeCompare(formEmailConfirmation.value) != 0){
+  if (formEmail.value.localeCompare(formEmailConfirmation.value) != 0) {
     alert('Emails son diferentes'); return;
   }
 
   // verifica el password
   let passCheckMsg = checkPassFormat(formPass.value);
-  if(passCheckMsg != ''){
+  if (passCheckMsg != '') {
     alert(passCheckMsg); return;
   }
 
   // comprobar que la contraseña y la confirmación de la contraseña son iguales
-  if(formPass.value.localeCompare(formPassConfirmation.value) != 0){
+  if (formPass.value.localeCompare(formPassConfirmation.value) != 0) {
     alert('El passwords es diferentes'); return;
   }
 
   // verifique que el correo electrónico y la contraseña sean iguales
-  if(formEmail.value.localeCompare(formPass.value) == 0){
+  if (formEmail.value.localeCompare(formPass.value) == 0) {
     alert('El correo electrónico y la contraseña no pueden ser iguales'); return;
   }
 
-  location.assign("prueba.html");
+  location.assign("tucine.html");
 
 
 
 
 
-// !-------------------comienzo LocalStorage
+  // !-------------------comienzo LocalStorage registro
 
-let clientes = 0;
-localStorage.setItem(`clientes: ${clientes}`, JSON.stringify({
-  "nombre": "",
-  "edad": "",
-  "userName": formUser.value,
-  "password": formPass.value,
-  "emails":  formEmail.value,
-  "foto": "",
-  "peliculas": [{
-    "titulo": "",
-    "techa": "",
-    "horario": "",
-    "nº entradas": "",
-    "sala": "",
+  let contador = 2;
+  localStorage.setItem(`clientes: ${contador}`, JSON.stringify({
+    "nombre": "",
+    "edad": "",
+    "userName": formUser.value,
+    "password": formPass.value,
+    "emails": formEmail.value,
+    "foto": "",
+    "peliculas": [{
+      "titulo": "",
+      "fecha": "",
+      "horario": "",
+      "nº entradas": "",
+      "sala": "",
 
-  }]
+    }]
 
-}))
+  }))
 
 
-  
+
 
 
 });
@@ -223,4 +255,3 @@ localStorage.setItem(`clientes: ${clientes}`, JSON.stringify({
 
 
 
-  
